ae0fa86e9fdf7ec38b27ab0161afd08664de04cc
Salesken.ai <anupsoni@salesken.ai>
2020-02-18 13:57:28 +0530

    initial commit for git update

---
+++ Pods/Target Support Files/Pods-Salesken-SaleskenUITests/Pods-Salesken-SaleskenUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Salesken_SaleskenUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Salesken_SaleskenUITests
+@end
---
+++ Pods/Target Support Files/Pods-Salesken/Pods-Salesken-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Salesken : NSObject
+@end
+@implementation PodsDummy_Pods_Salesken
+@end
