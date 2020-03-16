ca179df485a06126499106bc6620bca241983fd9
Anup <anupsoni@salesken.ai>
2020-03-16 16:25:23 +0530

    removed cocoa pods

--- Salesken/Helper/Sockets/Socket.swift
+++ Salesken/Helper/Sockets/Socket.swift
@@ -6,170 +6,82 @@
 //  Copyright © 2020 Salesken.ai. All rights reserved.
 //
 import Foundation
-import Combine
-//import Starscream
-//class Socket: NSObject, WebSocketDelegate{
-//    func didReceive(event: WebSocketEvent, client: WebSocket) {
-//        switch event {
-//        case .connected(let headers):
-//            isConnected = true
-//            print("websocket is connected: \(headers)")
-//            writeString(data: "Hi this is Anup soni")
-//        case .disconnected(let reason, let code):
-//            isConnected = false
-//            print("websocket is disconnected: \(reason) with code: \(code)")
-//        case .text(let string):
-//            print("Received text: \(string)")
-//        case .binary(let data):
-//            print("Received binary data: \(data.count)")
-//        case .ping(_):
-//            break
-//        case .pong(_):
-//            break
-//        case .viablityChanged(_):
-//            break
-//        case .reconnectSuggested(_):
-//            break
-//        case .cancelled:
-//            isConnected = false
-//        case .error(let error):
-//            isConnected = false
-//            handleError(error)
-//        }
-//    }
-//    func handleError(_ error: Error?) {
-//        if let e = error as? WSError {
-//            print("websocket encountered an error: \(e.message)")
-//        } else if let e = error {
-//            print("websocket encountered an error: \(e.localizedDescription)")
-//        } else {
-//            print("websocket encountered an error")
-//        }
-//    }
-//    var socket: WebSocket!
-//    var isConnected = false
-//
-//    static let instance = Socket()
-//    private override init(){
-//        super.init()
-//        var request = URLRequest(url: URL(string: Constants.SOCKET_URL)!)
-//        request.timeoutInterval = 5
-//        socket = WebSocket(request: request)
-//        socket.delegate = self
-//    }
-//
-//    func connect(){
-//        if !isConnected{
-//            socket.connect()
-//            print(socket.request)
-//            print("socket connecting")
-//        }
-//    }
-//    func disconnect(){
-//        if isConnected{
-//            socket.disconnect()
-//        }
-//    }
-//    func writeBinary(data: Data){
-//        if isConnected{
-//            socket.write(data: data)
-//            print("send audio data ",data)
-//        }
-//
-//    }
-//    func writeString(data: String){
-//        if isConnected{
-//            socket.write(string: data)
-//            print("send data ",data)
-//        }
-//
-//    }
-//}
-
-
-// web sockert
-protocol WebSocketConnection {
-    func send(text: String)
-    func send(data: Data)
-    func connect()
-    func disconnect()
-    var delegate: WebSocketConnectionDelegate? {
-        get
-        set
+import Starscream
+class Socket: NSObject, WebSocketDelegate{
+    func didReceive(event: WebSocketEvent, client: WebSocket) {
+        switch event {
+        case .connected(let headers):
+            isConnected = true
+            print("websocket is connected: \(headers)")
+            writeString(data: "Hi this is Anup soni")
+        case .disconnected(let reason, let code):
+            isConnected = false
+            print("websocket is disconnected: \(reason) with code: \(code)")
+        case .text(let string):
+            print("Received text: \(string)")
+        case .binary(let data):
+            print("Received binary data: \(data.count)")
+        case .ping(_):
+            break
+        case .pong(_):
+            break
+        case .viablityChanged(_):
+            break
+        case .reconnectSuggested(_):
+            break
+        case .cancelled:
+            isConnected = false
+        case .error(let error):
+            isConnected = false
+            handleError(error)
+        }
     }
-}
+    func handleError(_ error: Error?) {
+        if let e = error as? WSError {
+            print("websocket encountered an error: \(e.message)")
+        } else if let e = error {
+            print("websocket encountered an error: \(e.localizedDescription)")
+        } else {
+            print("websocket encountered an error")
+        }
+    }
+    var socket: WebSocket!
+    var isConnected = false
 
-protocol WebSocketConnectionDelegate {
-    func onConnected(connection: WebSocketConnection)
-    func onDisconnected(connection: WebSocketConnection, error: Error?)
-    func onError(connection: WebSocketConnection, error: Error)
-    func onMessage(connection: WebSocketConnection, text: String)
-    func onMessage(connection: WebSocketConnection, data: Data)
-}
-
-@available(iOS 13.0, *)
-class WebSocketTaskConnection: NSObject, WebSocketConnection, URLSessionWebSocketDelegate {
-    var delegate: WebSocketConnectionDelegate?
-    var webSocketTask: URLSessionWebSocketTask!
-    var urlSession: URLSession!
-    let delegateQueue = OperationQueue()
-    
-    init(url: URL) {
+    static let instance = Socket()
+    private override init(){
         super.init()
-        urlSession = URLSession(configuration: .default, delegate: self, delegateQueue: delegateQueue)
-        webSocketTask = urlSession.webSocketTask(with: url)
+        var request = URLRequest(url: URL(string: Constants.SOCKET_URL)!)
+        request.timeoutInterval = 5
+        socket = WebSocket(request: request)
+        socket.delegate = self
     }
-    
-    func urlSession(_ session: URLSession, webSocketTask: URLSessionWebSocketTask, didOpenWithProtocol protocol: String?) {
-        self.delegate?.onConnected(connection: self)
-    }
-    
-    func urlSession(_ session: URLSession, webSocketTask: URLSessionWebSocketTask, didCloseWith closeCode: URLSessionWebSocketTask.CloseCode, reason: Data?) {
-        self.delegate?.onDisconnected(connection: self, error: nil)
-    }
-    
-    func connect() {
-        webSocketTask.resume()
-        listen()
-    }
-    
-    func disconnect() {
-        webSocketTask.cancel(with: .goingAway, reason: nil)
-    }
-    
-    func listen()  {
-        webSocketTask.receive { result in
-            switch result {
-            case .failure(let error):
-                self.delegate?.onError(connection: self, error: error)
-            case .success(let message):
-                switch message {
-                case .string(let text):
-                    self.delegate?.onMessage(connection: self, text: text)
-                case .data(let data):
-                    self.delegate?.onMessage(connection: self, data: data)
-                @unknown default:
-                    fatalError()
-                }
-                
-                self.listen()
-            }
+
+    func connect(){
+        if !isConnected{
+            socket.connect()
+            print(socket.request)
+            print("socket connecting")
         }
     }
-    
-    func send(text: String) {
-        webSocketTask.send(URLSessionWebSocketTask.Message.string(text)) { error in
-            if let error = error {
-                self.delegate?.onError(connection: self, error: error)
-            }
+    func disconnect(){
+        if isConnected{
+            socket.disconnect()
         }
     }
-    
-    func send(data: Data) {
-        webSocketTask.send(URLSessionWebSocketTask.Message.data(data)) { error in
-            if let error = error {
-                self.delegate?.onError(connection: self, error: error)
-            }
+    func writeBinary(data: Data){
+        if isConnected{
+            socket.write(data: data)
+            print("send audio data ",data)
         }
+
+    }
+    func writeString(data: String){
+        if isConnected{
+            socket.write(string: data)
+            print("send data ",data)
+        }
+
     }
 }
+ 
